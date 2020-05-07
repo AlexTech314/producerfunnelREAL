17d858a88979ea0a7dab0d4574741ba38b40a43a
Alex Stepansky <alex.s@wustl.edu>
2020-05-07 17:40:10 -0400

    root react ready

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+function App() {
+  return (
+    <div className="App" id="main">
+     <h1>Where should we send your beats?</h1>
+    </div>
+  );
+}
+
+export default App;
